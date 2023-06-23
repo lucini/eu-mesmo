0a2d6abd3558ae70ee93080c428077641df40c93
Rafael Lucini <rafaellucini@gmail.com>
2023-06-23 15:56:31 -0300

    feat: primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'eu-mesmo';
+export class AppComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  position: "bottom" | "top" | "left" | "right" = 'left';
+
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Finder',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/finder.svg'
+      },
+      {
+        label: 'App Store',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/appstore.svg'
+      },
+      {
+        label: 'Photos',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/photos.svg'
+      },
+      {
+        label: 'Trash',
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/trash.png'
+      }
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DockModule } from 'primeng/dock';
+import { CardModule } from 'primeng/card';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DockModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
